4b10aebf8966055041997e60034fe5eb371009f9
voult001 <mathfla75@gmail.com>
2026-09-24 23:02:27 -0400

    Add main structure to Page component

--- app/page.tsx
+++ app/page.tsx
@@ -5,3 +5,17 @@ import { HowItWorks } from "@/components/how-it-works"
 import Pricing from "@/components/pricing"
 import SiteFooter from "@/components/site-footer"
 import ParallelNetwork from "@/components/parallel-network"
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-black">
+      <SiteHeader />
+      <HeroSection />
+      <FeatureGrid />
+      <HowItWorks />
+      <Pricing />
+      <ParallelNetwork />
+      <SiteFooter />
+    </main>
+  )
+}
